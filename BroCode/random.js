ef9afcdafcce69f7a5f6131a8009486c4ebcd20b
zarrin99 <zarrintasnim99@gmail.com>
2025-07-21 10:18:02 +0600

    completed
    completed random number generator

--- BroCode/random.js
+++ BroCode/random.js
@@ -1,8 +1,17 @@
+const submit = document.getElementById('submit');
+
+const result = document.getElementById('result');
 
 
-function random(){
+submit.onclick = function (){
+    min = document.getElementById('minValue').value;
+    max = document.getElementById('maxValue').value;
+    //console.log(min);
+    //console.log(max);
+
     let random;
     random = Math.floor( (Math.random() * max) + min );
     
+    result.textContent = random;
     
 }
